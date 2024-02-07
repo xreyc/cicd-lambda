22c9cc42b42ba3193b076ed9b216bba956ded776
xreyc <segumareyco@gmail.com>
2024-02-07 12:54:45 +0800

    redeploy

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ export const handler = async (event) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        id: 1,
-        name: "Reyco Seguma"
+        data: [
+          {
+            id: 1,
+            name: "Reyco Seguma"
+          },
+          {
+            id: 2,
+            name: "Kezeah Seguma"
+          }
+        ]
       }),
     };
     return response;
